470483392321b5c8c8f52aef893ef18cca0d8411
Matthew <Matthrews@outlook.com>
2021-03-21 00:45:38 +0800

    指定异步加载模块输出文件名

---
+++ webpack-demo-4/dist/670.js
@@ -0,0 +1,12 @@
+(self.webpackChunkwebpack_demo_4 = self.webpackChunkwebpack_demo_4 || []).push([[670], {
+    670: function (e, c, n) {
+        "use strict";
+        n.r(c), n.d(c, {
+            b: function () {
+                return u
+            }
+        });
+        var u = 12
+    }
+}]);
+//# sourceMappingURL=670.js.map
